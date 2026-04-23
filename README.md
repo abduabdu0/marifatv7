9bba72d84f1a32bd57b7191ac641380229e9c5d6
abduabdu <m7086054@gmail.com>
2026-04-23 15:20:23 +0500

    song

--- README.md
+++
@@ -1,2 +0,0 @@
-# marifatv5
-
--- song/playlist.js
+++ song/playlist.js
@@ -1,167 +1,172 @@
 ﻿window.PLAYLIST_DATA = [
     {
+        "title":  "Canım Xəstə - Bu Ürəyin Dərdi Sənsən (AI cover TikTok Trend)",
+        "artist":  "Atmosfeer",
+        "src":  "song/Canım Xəstə - Bu Ürəyin Dərdi Sənsən (AI cover TikTok Trend).mp3"
+    },
+    {
+        "title":  "Kerim_ve_Yıldız_klip_Омид_Амери_Дивоне_Манам_Omid_Ameri_Divane_Manam",
+        "artist":  "Омид Амери",
+        "src":  "song/Kerim_ve_Yıldız_klip_Омид_Амери_Дивоне_Манам_Omid_Ameri_Divane_Manam.mp3"
+    },
+    {
         "title":  "Muboriz_Usmonov_Rustam_Azimi_-_Surayo_79991422",
-        "artist":  "Muboriz_Usmonov_Rustam_Azimi",
+        "artist":  "Мубориз Усмонов & Рустам Азими",
         "src":  "song/Muboriz_Usmonov_Rustam_Azimi_-_Surayo_79991422.mp3"
     },
     {
-        "title": "Kerim_ve_Yıldız_klip_Омид_Амери_Дивоне_Манам_Omid_Ameri_Divane_Manam",
-        "artist": "Kerim_ve_Yıldız",
-        "src": "song/Kerim_ve_Yıldız_klip_Омид_Амери_Дивоне_Манам_Omid_Ameri_Divane_Manam.mp3"
-    },
-    {
         "title":  "Rustam_Azimi_-_Ajab_Tozai_79762391",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Ajab_Tozai_79762391.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Bevafo_79961027",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Bevafo_79961027.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Du-Du_79994599",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Du-Du_79994599.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Dukhtari_Chupon_79934547",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Dukhtari_Chupon_79934547.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Dunyoi_Ishq_80215010",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Dunyoi_Ishq_80215010.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Duston_79994578",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Duston_79994578.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Ey_Dust_80351984",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Ey_Dust_80351984.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Girya_Nakun_81166447",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Girya_Nakun_81166447.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Imshab_79779000",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Imshab_79779000.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Kabki_Dari_80479838",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Kabki_Dari_80479838.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Khushkhiromon_79934546",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Khushkhiromon_79934546.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Maryam_79779016",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Maryam_79779016.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Mastam_80434551",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Mastam_80434551.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Mastam_80473743",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Mastam_80473743.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Modar_79519428",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Modar_79519428.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Modar_79779020",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Modar_79779020.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Navruz_81086937",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Navruz_81086937.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Nigora_79515394",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Nigora_79515394.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Nimai_Jon_80215014",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Nimai_Jon_80215014.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Nosazo_79961031",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Nosazo_79961031.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Nozanin_80528582",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Nozanin_80528582.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Odate_79579345",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Odate_79579345.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Orom-Orom_Biyo_79542618",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Orom-Orom_Biyo_79542618.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Panjara_79400632",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Panjara_79400632.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Pushaymonam_79400620",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Pushaymonam_79400620.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Raqsi_Yor_81200102",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Raqsi_Yor_81200102.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Sabza_Ba_Noz_80461275",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Sabza_Ba_Noz_80461275.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Shahlo_79400592",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Shahlo_79400592.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Soyai_Dasti_Padar_79910907",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Soyai_Dasti_Padar_79910907.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Yod_Kardam_80412446",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Yod_Kardam_80412446.mp3"
     },
     {
         "title":  "Rustam_Azimi_-_Zam-Zama_80473745",
-        "artist":  "Rustam_Azimi",
+        "artist":  "Рустам Азими",
         "src":  "song/Rustam_Azimi_-_Zam-Zama_80473745.mp3"
     }
 ];
